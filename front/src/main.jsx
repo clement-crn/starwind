07b400b2a17a8b29bc7661d7d2a08be6bb86378d
Clément <clement@waven-consulting.fr>
2024-07-06 01:33:18 +0200

    init basic routes

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Root from "../src/routes/root.tsx"
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+])
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
