650c7c4355e28b5f60a0287b8297929530c5a736
Clément <clement@waven-consulting.fr>
2024-07-08 17:35:22 +0200

    b/handle login

--- djangobackend/myapp/urls.py
+++ djangobackend/myapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import RegisterView
+from .views import LoginAPIView, RegisterAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', RegisterAPIView.as_view(), name='register'),
+        path('login/', LoginAPIView.as_view(), name='login'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- djangobackend/myapp/views.py
+++ djangobackend/myapp/views.py
@@ -1,19 +1,35 @@
+# accounts/views.py
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import UserSerializer, RegisterSerializer,User
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer, UserSerializer
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = (permissions.AllowAny,)
+class RegisterAPIView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
 
-class ObtainTokenView(generics.GenericAPIView):
-    permission_classes = (permissions.AllowAny,)
-    
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = UserSerializer
+
     def post(self, request, *args, **kwargs):
-        refresh = RefreshToken.for_user(request.user)
+        username = request.data.get('username', None)
+        password = request.data.get('password', None)
+
+        user = User.objects.filter(username=username).first()
+
+        if user is None:
+            return Response({'error': 'Invalid credentials'}, status=400)
+
+        if not user.check_password(password):
+            return Response({'error': 'Invalid credentials'}, status=400)
+
+        serializer = UserSerializer(user)
+
+        refresh = RefreshToken.for_user(user)
+
         return Response({
             'refresh': str(refresh),
             'access': str(refresh.access_token),
+            'user': serializer.data
         })
