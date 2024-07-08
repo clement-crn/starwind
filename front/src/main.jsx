91742c60bef96cd5ef8ea63baa24cbf99d16d926
Clément <clement@waven-consulting.fr>
2024-07-08 21:36:41 +0200

    f/private root

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,14 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Root from "../src/routes/root.tsx"
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-])
+import Login from "../src/components/login/Login.tsx"
+import PrivateRoute from "../src/components/privateroot/PrivateRoot.tsx"
+import App from "./App.jsx"
 
+const router = createBrowserRouter([
+  { path: "/", element: <PrivateRoute component={App} /> },
+  { path: "/login", element: <Login /> },
+])
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
