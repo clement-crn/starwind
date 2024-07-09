39d5539e8f136b944689f006462a7f54bbf8053b
Clément <clement@waven-consulting.fr>
2024-07-09 19:55:27 +0200

    f/homepage placeholder

---
+++ front/src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Logout from "../../components/logout/Logout"
+
+const Home = () => {
+  return (
+    <>
+      <div>Homeeeeeeeeeeeeeeee</div>
+      <div>
+        <Logout />
+      </div>
+    </>
+  )
+}
+
+export default Home
