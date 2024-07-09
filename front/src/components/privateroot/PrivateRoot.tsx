0fc7c5c0b02feecfa7890d2e29339afb87ecf52a
Clément <clement@waven-consulting.fr>
2024-07-09 19:53:16 +0200

    f/handle private root

--- front/src/components/privateroot/PrivateRoot.tsx
+++ front/src/components/privateroot/PrivateRoot.tsx
@@ -1,12 +1,20 @@
-import React, { useState } from "react"
+import React, { PropsWithChildren, useEffect } from "react"
 import { Navigate } from "react-router-dom"
+import { useAuth } from "../../context/AuthContext"
 
-const PrivateRoute = ({ component: Component, authenticated, ...rest }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-  return isAuthenticated ? (
-    <Component {...rest} />
-  ) : (
-    <Navigate to="/login" replace />
-  )
+const PrivateRoute: React.FC = ({ children }: PropsWithChildren) => {
+  const { isAuthenticated } = useAuth()
+  useEffect(() => {
+    console.log("isAuthenticated", isAuthenticated)
+  }, [])
+
+  // Redirect to login if not authenticated
+  if (isAuthenticated === false) {
+    return <Navigate to="/login" />
+  }
+
+  // Render child routes
+  return children
 }
+
 export default PrivateRoute
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,17 +1,28 @@
-import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Login from "../src/components/login/Login.tsx"
-import PrivateRoute from "../src/components/privateroot/PrivateRoot.tsx"
+import Login from "./components/login/Login.tsx"
+import PrivateRoute from "./components/privateroot/index.ts"
 import App from "./App.jsx"
+import Home from "./pages/home/index.ts"
+import { AuthProvider } from "./context/AuthContext.tsx"
 
 const router = createBrowserRouter([
   { path: "/", element: <PrivateRoute component={App} /> },
   { path: "/login", element: <Login /> },
+  {
+    path: "/home",
+    element: (
+      <PrivateRoute>
+        <Home />
+      </PrivateRoute>
+    ),
+  },
 ])
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  //<React.StrictMode>
+  <AuthProvider>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </AuthProvider>
+  //</React.StrictMode>
 )
